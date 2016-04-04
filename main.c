d4bf7032564984b7d990dc7b648efc311bc293c4
Mohammad Haghighat <hosseinhg@hotmail.com>
2016-04-04 16:23:04 +0430

    Update main.c

--- main.c
+++ main.c
@@ -15,83 +15,88 @@
 
 void rand_init(SORT_TYPE *dst, SORT_TYPE max, size_t len) {
 
-    srand(time(NULL)); // start rand
-    size_t i;
+	srand(time(NULL)); // start rand
+	size_t i;
 
-    for (i = 0; i < len; i++)
-        dst[i] = rand() % max;
+	for (i = 0; i < len; i++)
+		dst[i] = rand() % max;
 }
 
 void print(SORT_TYPE *arr, size_t len) {
-//    return;
-    int idx;
+	//    return;
+	int idx;
 
-    for (idx = 0; idx < len; idx++) {
-        printf("%" PRT " ", arr[idx]);
-    }
+	for (idx = 0; idx < len; idx++) {
+		printf("%" PRT " ", arr[idx]);
+	}
 
-    printf("\n");
+	printf("\n");
 }
 
 size_t pow(size_t n, size_t p) {
-    size_t ret = 1, idx;
-    for (idx = 0; idx < p; idx++) {
-        ret *= n;
-    }
-    return ret;
+	size_t ret = 1, idx;
+	for (idx = 0; idx < p; idx++) {
+		ret *= n;
+	}
+	return ret;
 }
 
-double test(void (*func)(SORT_TYPE *, size_t), SORT_TYPE *dst, size_t len, size_t threshold) {
-    clock_t t1, t2;
+double test(void(*func)(SORT_TYPE *, size_t), SORT_TYPE *dst, size_t len, size_t threshold) {
+	clock_t t1, t2;
 
-    merge_sort_threshold = threshold;
-    t1 = clock();
-    func(dst, len);
-    t2 = clock();
+	merge_sort_threshold = threshold;
+	t1 = clock();
+	func(dst, len);
+	t2 = clock();
 
-    return (double) (t2 - t1) / CLOCKS_PER_SEC;
+	return (double)(t2 - t1) / CLOCKS_PER_SEC;
 }
 
 int main() {
 
-    SORT_TYPE *dst, *tmp_dst;
-    size_t i, t = 2;
-//    setlocale(LC_NUMERIC, "");
+	SORT_TYPE *dst, *tmp;
+	double *time_arr;
 
-    size_t len = pow(10, 5);
-    size_t max = 10 * len;
+	size_t i, t = 2;
+	setlocale(LC_NUMERIC, "");
 
-    dst = (SORT_TYPE *) malloc((size_t) sizeof(SORT_TYPE) * len);
-    tmp_dst = (SORT_TYPE *) malloc((size_t) sizeof(SORT_TYPE) * len);
+	size_t len = pow(10, 8);
+	size_t max = 10 * len;
 
-    rand_init(dst, max, len);
+	dst = (SORT_TYPE *)malloc((size_t) sizeof(SORT_TYPE) * len);
+	tmp = (SORT_TYPE *)malloc((size_t) sizeof(SORT_TYPE) * len);
 
-    memcpy(tmp_dst, dst, len * sizeof(SORT_TYPE));
+	rand_init(dst, max, len);
 
-    double insertion_sort_time = test(int64_binary_insertion_sort, tmp_dst, len, 0);
-
-    memcpy(tmp_dst, dst, len * sizeof(SORT_TYPE));
-    double merge_sort_time = test(int64_merge_sort, tmp_dst, len, 0);
-
-    for(i = 100; i < 400; i+=10){
-        memcpy(tmp_dst, dst, len * sizeof(SORT_TYPE));
-//        print(tmp_dst, len);
-        double merge_sort_with_threshold = test(int64_merge_sort, tmp_dst, len, i);
-        printf("merge sort t  : %'20d %'20d %20f \n", len, i, merge_sort_time);
-
-    }
-
-    printf("insertion sort: %20d %20d %20f \n", len, t, insertion_sort_time);
-    printf("merge sort    : %20d %20d %20f \n", len, t, merge_sort_time);
-
-    //print(tmp_dst, len);
-    printf("\n");
-    //print(dst, len);
+	memcpy(tmp, dst, len * sizeof(SORT_TYPE));
+	double merge_sort_time = test(int64_merge_sort, tmp, len, 0);
 
 
-    free(dst);
-    free(tmp_dst);
-    return 0;
+	double min = merge_sort_time;
+	size_t min_idx = 0;
+	double merge_t = 0;
+
+
+	//printf("insertion sort: %20d %20d %20f \n", len, t, insertion_sort_time);
+	printf("merge sort    : %20d %20d %20f \n", len, t, merge_sort_time);
+
+
+	for (i = 10; i < 30; i += 1) {
+		memcpy(tmp, dst, len * sizeof(SORT_TYPE));
+		merge_t = test(int64_merge_sort, tmp, len, i);
+		if (merge_t < min) {
+			min = merge_t;
+			min_idx = i;
+			//printf("merge sort t  : %20d %20d %20f \n", len, i, merge_t);
+			
+		}
+		
+	}
+
+	printf("best time: %f, index=%d\n", min, min_idx);
+	//print(time_arr, time_arr_len);
+	free(dst);
+	free(tmp);
+	getch();
+	return 0;
 }
-
-
